67ad76434ddb16c34685bc696e1c805562165969
Ye, Wenda <memorywilllast@sina.com>
2016-01-26 16:06:42 +0800

    hide pwd in backup_db_pic.sh

--- docs/linux/sh/backup_db_pic.sh
+++ docs/linux/sh/backup_db_pic.sh
@@ -2,12 +2,21 @@
 #--------------------------------------------------------------------------------------------------
 #@auther draguo
 #@20151230
+#
+#Hide mysql pwd
+#@auther Ye_WD
+#@20160126
 #--------------------------------------------------------------------------------------------------
 
 # 如果有错误进入vi set ff=unix
 export DB_NAME="casarover"
 export DB_USER="root"
-export DB_PASS=""
+export DB_PASS=$1
+
+if [ "$1"X == ""X ]
+then
+    echo "Warning: please input the mysql password. e.g. ./backup_db_pic.sh P@$$W0RD"
+fi
 
 cd /home/back
 Now=$(date +"%Y%m%d%H%M%S")
