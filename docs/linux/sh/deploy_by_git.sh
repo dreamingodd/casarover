debb0fd1eeff8e189d9d744ed8e5b83dce7de58d
Ye, Wenda <memorywilllast@sina.com>
2016-02-19 14:49:20 +0800

    backup DB in deployment, add some explicit tip in shell output.

--- docs/linux/sh/deploy_by_git.sh
+++ docs/linux/sh/deploy_by_git.sh
@@ -3,6 +3,8 @@
 #@auther dreamingodd(Ye, Wenda)
 #@20160121~28
 #Install/deploy casarover project, based on LAMP and Git.
+#@20160219
+#MySQL backup in /tmp and scripts execution whose log in /tmp.
 #--------------------------------------------------------------------------------------------------
 
 sys_time=`date "+%Y%m%d_%H%M%S"`
@@ -16,6 +18,7 @@ fi
 pwd=$1
 
 #Refresh git folder
+echo "----1.git processing"
 if [ -d /home/git ]
 then
     echo "Git folder exists."
@@ -29,6 +32,7 @@ else
 fi
 
 #Deploy casarover
+echo "----2.backing up PHP"
 cd /var/www/html/
 if [ -d /var/www/html/casarover ]
 then
@@ -60,10 +64,12 @@ else
     cp /home/git/casarover/docs/linux/config/index.html /var/www/html/index.html
     echo "Casarover folders and files are created."
 fi
+echo "----3.deploying PHP."
 cd casarover
 cp -r /home/git/casarover/application/ ./application
 cp -r /home/git/casarover/website/ ./website
-#Change DB password
+#Change DB password in PHP project
+echo "----4.Changing config in PHP project."
 mv /var/www/html/casarover/application/models/constant.php /var/www/html/casarover/application/models/constant.php.back
 echo "<?php" >> /var/www/html/casarover/application/models/constant.php
 echo "define('DB_HOST','localhost');" >> /var/www/html/casarover/application/models/constant.php
@@ -72,12 +78,19 @@ echo "define('DB_PWD','"${pwd}"');" >> /var/www/html/casarover/application/model
 echo "define('DB_NAME','casarover');" >> /var/www/html/casarover/application/models/constant.php
 echo "?>" >> /var/www/html/casarover/application/models/constant.php
 
-#Mysql script deployment
+#MySQL backup
+echo "----5.backing up DB..."
+backupFile=/tmp/DB_backup${sys_time}.sql
+mysqldump -uroot -p${pwd} casarover>${backupFile}
+
+#MySQL script deployment
+echo "----6.deploying SQLs"
 if [ -f /home/git/casarover/docs/sql/deployment/*.sql ]
 then
     for FILE in /home/git/casarover/docs/sql/deployment/*.sql
     do
-       mysql -uroot -p${pwd} -e "source $FILE" | tee /tmp/mysql_${sys_time}.sql
+        sys_time=`date "+%Y%m%d_%H%M%S"`
+        mysql -uroot -p${pwd} -e "source $FILE" | tee /tmp/DB_log_${sys_time}.sql
     done
 fi
 
