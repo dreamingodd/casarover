cb99e50916d3a894d0d5b922b3cfd07e938ce540
Ye, Wenda <memorywilllast@sina.com>
2016-02-23 10:22:11 +0800

    change mysql pwd sql

--- docs/linux/sh/change_default_mysql_password.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#--------------------------------------------------------------------------------------------------
-#@auther dreamingodd(Ye, Wenda)
-#@20160223
-#Change the password of new installed mysql. (default password is null)
-#Must restart mysqld service.
-#--------------------------------------------------------------------------------------------------
-use mysql;
-update user set Password=PASSWORD('24') where User='root';
---
+++ docs/sql/change_default_mysql_password.sql
@@ -0,0 +1,6 @@
+/*@auther dreamingodd(Ye, Wenda)*/
+/*@20160223*/
+/*Change the password of new installed mysql. (default password is null)*/
+/*Must restart mysqld service.*/
+use mysql;
+update user set Password=PASSWORD('24') where User='root';
