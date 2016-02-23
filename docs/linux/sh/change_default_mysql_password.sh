dd09d9e8066928aa6c078a407d187709b4672625
Ye, Wenda <memorywilllast@sina.com>
2016-02-23 10:18:21 +0800

    slight--git shell

--- docs/linux/sh/change_default_mysql_password.sh
+++ docs/linux/sh/change_default_mysql_password.sh
@@ -1,14 +1,9 @@
 #!/bin/bash
 #--------------------------------------------------------------------------------------------------
 #@auther dreamingodd(Ye, Wenda)
-#@20160120
-#Install git.
 #@20160223
-#This shell may run failed, if so, you will have to run it line by line.
+#Change the password of new installed mysql. (default password is null)
+#Must restart mysqld service.
 #--------------------------------------------------------------------------------------------------
-
-yum -y install git-core
-git config --global user.name "Ye_WD"
-git config --global user.email memorywilllast@sina.com
-
-echo "Git installation completed."
+use mysql;
+update user set Password=PASSWORD('24') where User='root';
---
+++ docs/linux/sh/install_git_preceding.sh
@@ -0,0 +1,2 @@
+#Git related
+yum -y install curl-devel expat-devel gettext-devel openssl-devel zlib-devel
