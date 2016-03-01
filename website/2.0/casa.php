fc8980d5308838a5e07fbf381d0af7fb0ba24459
van <draguo@sina.com>
2016-03-01 15:29:04 +0800

    首页完善

---
+++ website/2.0/casa.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="zh-cn">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=no">
+    <meta name="keywords" content="民宿,杭州,周末,去哪玩">
+    <meta name="description" content="找到好民宿">
+    <title>{民宿的名字}-民宿</title>
+    <link rel="stylesheet" href="css/main.css">
+    <!--[if lt IE 9]>
+    <script src="http://cdn.bootcss.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+    <script src="http://cdn.bootcss.com/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+    <script src="//cdn.bootcss.com/jquery/2.1.1/jquery.min.js"></script>
+    <script src="js/integration/jquery.flexslider-min.js" type="text/javascript"></script>
+    <script src="js/home.js" type="text/javascript"></script>
+</head>
+<body>
+<header>
+    <nav class="navbar navbar-default">
+        <!-- logo -->
+        <div class="nav-left">
+            <a href="#">
+                <img src="images/logo.png" alt="logo">
+            </a>
+        </div>
+        <div class="nav-middle">
+            <a href="">民宿大全</a>
+            <a href="#recom">民宿推荐</a>
+            <a href="#theme">精选主题</a>
+            <a href="#series">探庐系列</a>
+        </div>
+        <div class="nav-right">
+            <a href=""></a>
+            <a href="">登录</a>
+            <a href="">注册</a>
+        </div>
+    </nav> 
+    <div class="casa-head">
+        <div class="cover-photo">
+            <img src="images/head.png" width="100%" alt="">
+        </div>
+        <div class="show-mess">
+            <div class="mark">浏览233</div>
+            <div class="title">杭州青逸假日民宿</div>
+        </div>
+
+    </div>
+    <!-- 民宿介绍内容 -->
+    <article>
+        <div class="article-main">
+            成都，是一座让人感到幸福的城市，也是莲安遇到Curt的城市。莲安，成都莲公馆的主人，在开民宿之前，做过金融做过贸易，也曾经在这个行业很有抱负。直到她遇到了Curt，生活轨迹便由此改变。Curt，标准的美国理工男，是一名化学工程师，常常在世界各地跑。两人一个热爱文学、音乐和摄影，一个是理性宅男，但他们共同的爱好就是一起旅行，寻找生活之美。
+        </div>
+    </article>
+<?php include 'footer.php' ?>
+</body>
+</html>
