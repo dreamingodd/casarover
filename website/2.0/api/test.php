9fe97a8522a1d46eaede3ca98c3bb64e6683cef7
van <draguo@sina.com>
2016-03-02 16:16:41 +0800

    修改名字

--- website/2.0/api/test.php
+++ website/2.0/api/test.php
@@ -1,7 +1,7 @@
 <?php
 $val = array("message" => "第一个","short"=>"这个是介绍","pic"=>"images/fang.jpg");
 $val2 = array("message" => "第二个" , "short"=>"这个是第二个介绍","pic"=>"images/fang.jpg");
-$data = array($val);
+$data = array($val,$val2);
 
 $json_data = json_encode($data);
 echo $json_data;
