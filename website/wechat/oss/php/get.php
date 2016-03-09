f3f9734b825e264074be4add22c6be53813a7147
Ye, Wenda <memorywilllast@sina.com>
2016-03-09 14:22:10 +0800

    --Bug no longer use DateTime

--- website/wechat/oss/php/get.php
+++ website/wechat/oss/php/get.php
@@ -2,9 +2,7 @@
 <?php include_once 'oss_php_sdk_20140625/sdk.class.php';?>
 <?php
     function gmt_iso8601($time) {
-        $dtStr = date("c", $time);
-        $mydatetime = new DateTime($dtStr);
-        $expiration = $mydatetime->format(DateTime::ISO8601);
+        $expiration = date('Y-m-d\TH:i:sO', $time);
         $pos = strpos($expiration, '+');
         $expiration = substr($expiration, 0, $pos);
         return $expiration."Z";
