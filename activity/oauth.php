0d4de6834b7e2bd26a6f648332a6b7ab396906ac
van <draguo@sina.com>
2016-03-10 11:13:37 +0800

    activity

--- activity/oauth.php
+++ activity/oauth.php
@@ -1,20 +1,20 @@
 <?php
 // Get wechat openid 获取微信openid
-$code = $_GET['code'];
-$appid = "wxeafd79d8fcbd74ee";
-$secret = "5db9a898bdd7f430bbc563476021f4b2";
-$get_token_url = 'https://api.weixin.qq.com/sns/oauth2/access_token?appid='.$appid.'&secret='
-    .$secret.'&code='.$code.'&grant_type=authorization_code';
-$ch = curl_init();
-curl_setopt($ch,CURLOPT_URL,$get_token_url);
-curl_setopt($ch,CURLOPT_HEADER,0);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1 );
-curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 10);
-$res = curl_exec($ch);
-curl_close($ch);
-$json_obj = json_decode($res,true);
-$access_token = $json_obj['access_token'];
-$openid = $json_obj['openid'];
+ $code = $_GET['code'];
+ $appid = "wxeafd79d8fcbd74ee";
+ $secret = "5db9a898bdd7f430bbc563476021f4b2";
+ $get_token_url = 'https://api.weixin.qq.com/sns/oauth2/access_token?appid='.$appid.'&secret='
+     .$secret.'&code='.$code.'&grant_type=authorization_code';
+ $ch = curl_init();
+ curl_setopt($ch,CURLOPT_URL,$get_token_url);
+ curl_setopt($ch,CURLOPT_HEADER,0);
+ curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1 );
+ curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 10);
+ $res = curl_exec($ch);
+ curl_close($ch);
+ $json_obj = json_decode($res,true);
+ $access_token = $json_obj['access_token'];
+ $openid = $json_obj['openid'];
 
 // echo $openid;
 
