7a2ef07499292a6d24969fce8c2782c622731402
van <draguo@sina.com>
2016-03-10 10:28:20 +0800

    jssdk测试

--- activity/php/sample.php
+++ activity/php/sample.php
@@ -36,7 +36,14 @@ $signPackage = $jssdk->GetSignPackage();
     ]
   });
   wx.ready(function () {
-    // 在这里调用 API
+    wx.checkJsApi({
+      jsApiList: ['chooseImage'], // 需要检测的JS接口列表，所有JS接口列表见附录2,
+      success: function(res) {
+        // 以键值对的形式返回，可用的api值true，不可用为false
+        // 如：{"checkResult":{"chooseImage":true},"errMsg":"checkJsApi:ok"}
+        console.log(res);
+      }
+    });
   });
 </script>
 </html>
