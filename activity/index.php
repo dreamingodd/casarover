d17364e7839ea86cb43d757b050c2c25b69ee8f6
van <draguo@sina.com>
2016-03-11 13:47:31 +0800

    修改书写错误

--- activity/index.php
+++ activity/index.php
@@ -8,6 +8,6 @@ $wxapi = new WXapi();
 $loc = $_GET["loc"];
 //进入之后进入微信的授权页面获取code
 //成功之后的跳转链接
-$redirect_uri = 'http://www.casarover.com/casarover/activity/oauth.php?loc'=$loc;
+$redirect_uri = 'http://www.casarover.com/casarover/activity/oauth.php?loc='.$loc;
 $start_stup = $wxapi->start($redirect_uri);
 
